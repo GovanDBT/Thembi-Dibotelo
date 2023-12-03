6e67c348b35165f7afcca17875a1f5e1ec2e257f
Thembi Dibotelo <govan.dibotelo@gmail.com>
2023-12-03 09:54:15 +0200

    add an autotype to the subtitle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+// Navigation bar collapsible
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible-expanded");
   })
 );
+
+// Auto type animation
+var options = {
+    strings: ["Websites", "Apps", "Mockups", "Logos"],
+    typeSpeed: 150,
+    backspeed: 150,
+    loop: true
+};
+    
+var typed = new Typed('.auto-type', options);
