a78ed5a8d2ecd8045d7caade54052ebe4b2a8cfc
Thembi Dibotelo <govan.dibotelo@gmail.com>
2023-12-05 16:06:19 +0200

    create the my designs showcase and caurosel function

--- js/main.js
+++ js/main.js
@@ -15,3 +15,48 @@ var options = {
 };
     
 var typed = new Typed('.auto-type', options);
+
+// Swiper carousel
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+   delay: 3000,
+  },
+  effect: 'coverflow',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    }
+  },
+});
