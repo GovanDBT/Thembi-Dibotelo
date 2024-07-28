f65e068d4d5745e96a3d8ba005f4bf6ef1a578cd
Thembi Dibotelo <govan.dibotelo@gmail.com>
2024-07-28 02:16:00 +0200

    change slider type

--- js/main.js
+++ js/main.js
@@ -18,47 +18,22 @@ var typed = new Typed('.auto-type', options);
 
 // Swiper carousel
 
-const swiper = new Swiper ('.design-swiper', {
-  // Optional parameters
-  loop: true,
-  autoplay: {
-   delay: 3000,
-  },
-  effect: 'coverflow',
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-
-  // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 20,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    1024: {
+var swiper = new Swiper(".design-swiper", {
+      loop: true,
+      autoplay: true,
       slidesPerView: 3,
-      spaceBetween: 60
-    }
-  },
+      spaceBetween: 30,
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 });
 
 const swiper2 = new Swiper('.testimonial-swiper', {
